3ed38205bef46ce22619b95db550610e6d9ef20f
Chris Morrison <chris-morrison@cyberservices.com>
2015-02-03 01:55:55 +0000

    Added WebAddressBar control

---
+++ dotNET/WebAddressBar.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Panoptimus
+{
+    partial class WebAddressBar
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.addressBox = new System.Windows.Forms.TextBox();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // addressBox
+            // 
+            this.addressBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.addressBox.Location = new System.Drawing.Point(0, 0);
+            this.addressBox.Name = "addressBox";
+            this.addressBox.Size = new System.Drawing.Size(368, 20);
+            this.addressBox.TabIndex = 0;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(481, 2);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(16, 16);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox.TabIndex = 1;
+            this.pictureBox.TabStop = false;
+            // 
+            // WebAddressBar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.addressBox);
+            this.Name = "WebAddressBar";
+            this.Size = new System.Drawing.Size(500, 20);
+            this.Load += new System.EventHandler(this.WebAddressBar_Load);
+            this.Resize += new System.EventHandler(this.WebAddressBar_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox addressBox;
+        private System.Windows.Forms.PictureBox pictureBox;
+    }
+}
---
+++ dotNET/WebAddressBar.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Panoptimus
+{
+    public partial class WebAddressBar : UserControl
+    {
+        const float BUTTON_WIDTH = 32.00f;
+        const float CERT_AREA = 60.00f;
+
+        public WebAddressBar()
+        {
+            InitializeComponent();
+        }
+
+        private void WebAddressBar_Resize(object sender, EventArgs e)
+        {
+
+        }
+
+        private void WebAddressBar_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
